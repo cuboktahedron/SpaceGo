7e04713f85e1a25e832fdcb908abd1960f69536d
cuboktahedron <cuboktahedron@gmail.com>
2016-07-17 21:29:16 +0900

    add browser auto reload

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var plumber = require('gulp-plumber');
 var sourcemaps = require('gulp-sourcemaps');
 var webpack = require('gulp-webpack');
 var spawn = require('child_process').spawn;
+var livereload = require('gulp-livereload');
 
 gulp.task('cjs',function(){
   gulp.src(['src/scripts/app/client/*.js'])
@@ -28,13 +29,27 @@ gulp.task('server',function(){
   server = spawn('node',['./bin/www']);
 })
 
-gulp.task('watch', function () {
+gulp.task('watch-client', function () {
   gulp.watch('src/sass/**/*.scss', ['sass']);
   gulp.watch('src/scripts/app/client/**/*.js', ['cjs']);
+});
+
+gulp.task('watch-server', ['server'], function() {
+  livereload.listen();
   gulp.watch([
     'app.js',
+    'routes/**/*.js',
     'src/scripts/app/server/**/*.js'
   ], ['server']);
+
+  gulp.watch(['public/**/*', 'views/**/*'], ['reload']);
 });
+
+gulp.task('watch', ['watch-client', 'watch-server']);
+
+gulp.task('reload',function(){
+  gulp.src(['public/**/*','views/**/*'])
+    .pipe(livereload());
+})
 
 gulp.task('default', ['cjs', 'sass']);
