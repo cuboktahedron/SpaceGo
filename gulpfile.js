f5c1cf3c58c5a0c8fce64832c8854233304b9826
cuboktahedron <cuboktahedron@gmail.com>
2016-07-17 20:23:24 +0900

    add server watching and restart

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
 var sourcemaps = require('gulp-sourcemaps');
 var webpack = require('gulp-webpack');
+var spawn = require('child_process').spawn;
 
 gulp.task('cjs',function(){
   gulp.src(['src/scripts/app/client/*.js'])
@@ -19,9 +20,21 @@ gulp.task('sass',function(){
     .pipe(gulp.dest('public/stylesheets'));
 });
 
+var server = null;
+gulp.task('server',function(){
+  if(server){
+    server.kill('SIGKILL');
+  }
+  server = spawn('node',['./bin/www']);
+})
+
 gulp.task('watch', function () {
   gulp.watch('src/sass/**/*.scss', ['sass']);
   gulp.watch('src/scripts/app/client/**/*.js', ['cjs']);
+  gulp.watch([
+    'app.js',
+    'src/scripts/app/server/**/*.js'
+  ], ['server']);
 });
 
 gulp.task('default', ['cjs', 'sass']);
