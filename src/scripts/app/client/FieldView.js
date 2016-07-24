03f49bb317a54ac85adb6efbd5b4f723cd9055f3
cuboktahedron <cuboktahedron@gmail.com>
2016-07-24 20:01:20 +0900

    draw virtual board part

--- src/scripts/app/client/FieldView.js
+++ src/scripts/app/client/FieldView.js
@@ -14,7 +14,10 @@ define(function(require) {
     init: function() {
       this._$canvas = $('#field');
       this._canvas = $('#field')[0];
+      this._$backCanvas = $('#back-field');
+      this._backCanvas = $('#back-field')[0];
       this._ctx = this._canvas.getContext('2d');
+      this._backCtx = this._backCanvas.getContext('2d');
       this._height = this._canvas.width;
       this._width = this._canvas.height;
 
@@ -37,31 +40,80 @@ define(function(require) {
     },
 
     _refreshAll: function(pl) {
+      this._clearBoard(this._backCtx);
+      this._clearBoard(this._ctx);
       this._writeBoard(pl);
       this._writeStars(pl);
       this._writeStones(pl);
+
+      var unit = 45;
+      var halfUnit = unit / 2;
+      var margin = 204;
+      var unit4 = unit * 4;
+      var unit5 = unit * 5;
+      var unit8 = unit * 8;
+      var unit9 = unit * 9;
+
+      this._ctx.save();
+      this._ctx.globalAlpha = 0.4;
+      this._ctx.fillStyle = 'rgb(0, 255, 255)';
+      this._ctx.fillRect(0, 0, this._width, this._height);
+      this._ctx.restore();
+
+      this._ctx.save();
+      this._ctx.globalAlpha = 0.7;
+
+      // left-up
+      this._ctx.drawImage(this._backCanvas, margin + unit5 - halfUnit, margin + unit5 - halfUnit, unit4, unit4, 0, 0, unit4, unit4);
+
+      // right-up
+      this._ctx.drawImage(this._backCanvas, margin - halfUnit, margin + unit5 - halfUnit, unit4, unit4, margin + unit8 + halfUnit, 0, unit4, unit4);
+
+      // left-down
+      this._ctx.drawImage(this._backCanvas, margin + unit5 - halfUnit, margin - halfUnit, unit4, unit4, 0, margin + unit8 + halfUnit, unit4, unit4);
+
+      // right-down
+      this._ctx.drawImage(this._backCanvas, margin - halfUnit, margin - halfUnit, unit4, unit4, margin + unit8 + halfUnit, margin + unit8 + halfUnit, unit4, unit4);
+
+      // up
+      this._ctx.drawImage(this._backCanvas, margin - halfUnit, margin + unit5 - halfUnit, unit9, unit4, margin - halfUnit, 0, unit9, unit4);
+
+      // left
+      this._ctx.drawImage(this._backCanvas, margin + unit5 - halfUnit, margin - halfUnit, unit4, unit9, 0, margin - halfUnit, unit4, unit9);
+
+      // down
+      this._ctx.drawImage(this._backCanvas, margin - halfUnit, margin - halfUnit, unit9, unit4, margin - halfUnit, margin + unit8 + halfUnit, unit9, unit4);
+
+      // right
+      this._ctx.drawImage(this._backCanvas, margin - halfUnit, margin - halfUnit, unit4, unit9, margin + unit8 + halfUnit, margin - halfUnit, unit4, unit9);
+
+      this._ctx.restore();
+      this._ctx.drawImage(this._backCanvas, margin - halfUnit, margin - halfUnit, unit9, unit9, margin - halfUnit, margin - halfUnit, unit9, unit9);
+    },
+
+    _clearBoard: function(ctx) {
+      ctx.fillStyle = 'rgb(0, 0, 0)';
+      ctx.fillRect(0, 0, this._width, this._height);
     },
 
     _writeBoard: function(pl) {
-      var c = this._ctx;
+      var c = this._backCtx;
       var x, y;
       var unit = 45;
+      var halfUnit = unit / 2;
       var margin = 204;
-
-      c.fillStyle = 'rgb(0, 0, 0)';
-      c.fillRect(0, 0, this._width, this._height);
 
       c.beginPath();
       c.strokeStyle = 'rgb(255, 255, 255)';
       c.lineWidth = 3;
       for (x = 0; x < pl.field.size; x++) {
-        c.moveTo(unit * x + margin, margin);
-        c.lineTo(unit * x + margin, this._height - margin);
+        c.moveTo(unit * x + margin, margin - halfUnit);
+        c.lineTo(unit * x + margin, this._height - margin + halfUnit);
       }
 
       for (y = 0; y < pl.field.size; y++) {
-        c.moveTo(margin,               unit * y + margin);
-        c.lineTo(this._width - margin, unit * y + margin);
+        c.moveTo(margin - halfUnit,               unit * y + margin);
+        c.lineTo(this._width - margin + halfUnit, unit * y + margin);
       }
 
       c.closePath();
@@ -69,7 +121,7 @@ define(function(require) {
     },
 
     _writeStars: function(pl) {
-      var c = this._ctx;
+      var c = this._backCtx;
       var i;
       var x, y;
       var xx, yy;
@@ -77,6 +129,7 @@ define(function(require) {
       var margin = 204;
       var stars = [
         { x: 2, y:2 },
+        { x: 4, y:4 },
         { x: 6, y:2 },
         { x: 4, y:4 },
         { x: 2, y:6 },
@@ -98,7 +151,7 @@ define(function(require) {
     },
 
     _writeStones: function(pl) {
-      var c = this._ctx;
+      var c = this._backCtx;
       var x, y;
       var xx, yy;
       var unit = 45;
