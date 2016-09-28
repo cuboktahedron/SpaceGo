5791321dd7965e5c4c94ffc9f0ea7ae84146c50a
cuboktahedron <cuboktahedron@gmail.com>
2016-09-28 11:40:17 +0900

    implement ban suicide

--- src/scripts/app/client/domain/model/Board.js
+++ src/scripts/app/client/domain/model/Board.js
@@ -52,10 +52,6 @@ define(function(require) {
       return false;
     }
 
-    if (this.existsStone(x, y)) {
-      return false;
-    }
-
     if (this.isBanPoint(x, y, stone)) {
       return false;
     }
@@ -71,9 +67,104 @@ define(function(require) {
     return this._coodinates[x][y] != null;
   };
 
-  Board.prototype.isBanPoint = function(x, y) {
-    // TODO: not implemented
+  Board.prototype.isBanPoint = function(x, y, stone) {
+    if (this.existsStone(x, y)) {
+      return false;
+    }
+
+    // TODO: ban recapuring ko immediately
+
+    if (this.dryPutStone(x, y, stone) === -1) {
+      return true;
+    }
+
     return false;
+  };
+
+  Board.prototype.dryPutStone = function(x, y, stone) {
+    // TODO: make copy method
+    var captureData;
+    var dryBoard = new Board(this.size);
+    var xi, yi;
+    for (xi = 0; xi < dryBoard.size; xi++) {
+      for (yi = 0; yi < dryBoard.size; yi++) {
+        dryBoard._coodinates[xi][yi] = this._coodinates[xi][yi];
+      }
+    }
+
+    dryBoard._coodinates[x][y] = stone;
+
+    captureData = dryBoard._capture(x, y, stone);
+    if (captureData.num === 0) {
+      if(dryBoard._isSurroundedByEnemy(x, y, stone)) {
+        return -1;
+      }
+    }
+
+    return captureData.num;
+  };
+
+  Board.prototype._capture = function(x, y, myStone) {
+    // TODO: not impelemented
+    var data = {};
+
+    data.num = 0;
+    return data;
+  };
+
+  Board.prototype._isSurroundedByEnemy = function(x, y, myStone) {
+    var that = this;
+    var searched = [];
+    var x, y;
+    var xi, yi;
+    var cc = this._correctCoordinate;
+    var searchSpace = function(xx, yy) {
+      if (searched[xx][yy]) {
+        console.log('a');
+        return false;
+      }
+
+      if (that._coodinates[xx][yy] == null) {
+        return true;
+      }
+
+      searched[xx][yy] = true;
+
+      if (that._coodinates[xx][yy].equals(myStone)) {
+        if (searchSpace(that._correctCoordinate(xx - 1), yy)) return true;
+        if (searchSpace(xx, that._correctCoordinate(yy - 1))) return true;
+        if (searchSpace(that._correctCoordinate(xx + 1), yy)) return true;
+        if (searchSpace(xx, that._correctCoordinate(yy + 1))) return true;
+      }
+
+      return false;
+    }
+
+    for (xi = 0; xi < this.size; xi++) {
+      searched[xi] = [];
+      for (yi = 0; yi < this.size; yi++) {
+        searched[xi][yi] = false;
+      }
+    }
+
+    if (searchSpace(x, y)) {
+      return false;
+
+    }
+
+    return true;
+  };
+
+  Board.prototype._correctCoordinate = function(n) {
+    while (n < 0) {
+      n = n + this.size;
+    }
+
+    if (n >= this.size) {
+      n = n % this.size;
+    }
+
+    return n;
   };
 
   Board.prototype.getState = function() {
--- src/scripts/test/client/domain/model/Board.test.js
+++ src/scripts/test/client/domain/model/Board.test.js
@@ -2,6 +2,7 @@ define(function(require) {
 
   var Stone = require('app/client/domain/model/Stone');
   var Board = require('app/client/domain/model/Board');
+  var BoardUtil;
 
   describe('Board', function() {
     it("returns size", function() {
@@ -32,6 +33,45 @@ define(function(require) {
 
       assert.isNull(board.getStone(1, 2));
     });
+
+    it("rejects putting stone at suicide point", function() {
+      var N = null;
+      var B = Stone.Black;
+      var W = Stone.White;
+
+      var board = BoardUtil.setupBoard([
+        [ N, W, W, W ],
+        [ W, N, W, N ],
+        [ N, W, W, W ],
+        [ W, W, N, W ],
+      ]);
+
+      assert.isFalse(board.canPutStone(0, 0, B));
+      assert.isFalse(board.canPutStone(1, 1, B));
+      assert.isFalse(board.canPutStone(3, 1, B));
+      assert.isFalse(board.canPutStone(0, 2, B));
+      assert.isFalse(board.canPutStone(2, 3, B));
+    });
   });
+
+  BoardUtil = {};
+  BoardUtil.setupBoard = function(boardData) {
+    var board = new Board(boardData.length);
+    var x, y;
+
+    for (x = 0; x < board.size; x++) {
+      for (y = 0; y < board.size; y++) {
+        if (boardData[y][x] == null) {
+          continue;
+        }
+
+        if (!board.putStone(x, y, boardData[y][x])) {
+          throw new Error("Invalid boardData passed.");
+        }
+      }
+    }
+
+    return board;
+  };
 });
 
